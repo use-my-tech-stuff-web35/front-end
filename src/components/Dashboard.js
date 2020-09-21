a639663b53f0c74143cf574c1efbe0ddd8f8ed24
willwearing <willwearing@gmail.com>
2020-09-21 14:07:20 -0600

    fixed rendering issue

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,23 +1,29 @@
-import React from 'react';
-import * as yup from 'yup';
-import { axiosWithAuth } from '../utils/axiosWithAuth';
-import Header from './Header';
-import Signup from './SignUp';
-import Login from './Login';
-import formSchema from './formSchema';
-import { BrowserRouter as Router, Route, Link, useHistory } from 'react-router-dom';
-import PrivateRoute from './PrivateRoute';
-import TechRentalList from './TechRentalList';
-import AddNewTech from './AddNewTech';
-import EditTechItem from './EditTechItem';
-
+import React from "react";
+import * as yup from "yup";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+import Header from "./Header";
+import SignUpComponent from "./SignUpComponent";
+import Login from "./Login";
+import formSchema from "./formSchema";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  useHistory,
+} from "react-router-dom";
+import PrivateRoute from "./PrivateRoute";
+import TechRentalList from "./TechRentalList";
+import AddNewTech from "./AddNewTech";
+import EditTechItem from "./EditTechItem";
+// import SignUpFormComponent from "./SignUpFormComponent";
 
 const Dashboard = () => {
-    return (
-        <Router>
-			<Header />
-        </Router>
-    )
-}
+  return (
+    <Router>
+      <Header />
+      <Route path="/signup" component={() => <SignUpComponent />} />
+    </Router>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
---
+++ src/components/formSchema.js
@@ -0,0 +1,20 @@
+import React from "react";
+import * as yup from "yup";
+
+//schema for SignUpForm
+export default yup.object().shape({
+  username: yup
+    .string()
+    .test(
+      "length",
+      "Enter a username that is longer than 2 Characters",
+      (value) => value.length > 2
+    ),
+  password: yup
+    .string()
+    .test(
+      "length",
+      "You're password must be 8 characters or more",
+      (value) => value.length >= 8
+    ),
+});
