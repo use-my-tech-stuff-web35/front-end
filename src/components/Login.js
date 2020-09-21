a25ee6b24ddcb30be5efead86ad6cda78abbfbf2
mike hill <michael-hill@lambdastudents.com>
2020-09-21 16:24:57 -0400

    login form working

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+import { useHistory } from "react-router-dom"
+
+
+const initialFormValues = {
+    username:'',
+    password:'',
+  }
+
 
 const Login = () => {
+    const [formValues, setFormValues] = useState(initialFormValues)
+    const newPlace = useHistory()
+
+
+    const formSubmit = (evt) => {
+        evt.preventDefault()
+        const user = {
+          username: formValues.username.trim(),
+          password: formValues.password.trim(),
+        }
+        console.log(user)
+        axios.post('https://used-tech.herokuapp.com/api/auth/login', user)
+        .then(res => {
+          newPlace.push("/techlist")
+          setFormValues(initialFormValues)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+      }
+
+      const onChange = (evt) => {
+        const { name, value }= evt.target
+        setFormValues({ ...formValues, [name]: value })
+      }
+
     return (
+        <form onSubmit={formSubmit}>
         <div>
-            
+            <h2>Login here!</h2>
+            <div>
+                <label>Username:
+                    <input 
+                    value={formValues.username}
+                    onChange={onChange}
+                    name='username'
+                    type='text'
+                    placeholder='Enter Username'
+                    />
+                </label>
+                <label>Password:
+                    <input 
+                    value={formValues.password}
+                    onChange={onChange}
+                    name='password'
+                    type='text'
+                    placeholder='Enter Password'
+                    />
+                </label>
+            </div>
+                <button id='loginBtn'>Login</button>
         </div>
+        </form>
     )
 }
 
