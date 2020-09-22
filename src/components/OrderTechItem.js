331d80fbf297aeddc15dcdec9208d7d49526c636
willwearing <willwearing@gmail.com>
2020-09-22 11:02:48 -0600

    added order form and styled components with some basic hover animations

--- src/components/OrderTechItem.js
+++ src/components/OrderTechItem.js
@@ -1,6 +1,44 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { act } from "react-dom/test-utils";
+import styled from "styled-components";
+
+//component styling
+const StyledDiv = styled.div`
+  background-image: url("https://images.unsplash.com/photo-1505424297051-c3ad50b055ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80");
+  background-size: cover;
+  background-position: center;
+  height: 1000px;
+  background-repeat: no-repeat;
+  select {
+    width: 15%;
+    padding: 12px 20px;
+    margin: 8px 1%;
+  }
+  button {
+    max-width: 292px;
+    width: 200px;
+    margin-top: 1%;
+    margin-bottom: 20px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    border: 2px solid #fff;
+    border-radius: 5px;
+    background-color: #102542;
+    transition: color 0.2s ease, background-color 0.2s ease;
+    font-family: Inter, sans-serif;
+    cursor: pointer;
+    color: #f87060;
+    font-size: 18px;
+    line-height: 160%;
+    font-weight: 700;
+    text-decoration: none;
+  }
+  button:hover {
+    background-color: #b3a394;
+    color: #102542;
+  }
+`;
 
 const initialFormValues = {
   dropdown: [],
@@ -39,10 +77,12 @@ const OrderTechItem = () => {
   }, []);
 
   return (
-    <div>
+    <StyledDiv>
       <h2>Enter your details to order an item!</h2>
       <form onSubmit={submit}>
-        <label>Choose an item:</label>
+        <label>
+          <strong>Choose an item:</strong>
+        </label>
         <select name="choice" onChange={onChange}>
           {formValues &&
             formValues.dropdown.map((item) => {
@@ -51,7 +91,7 @@ const OrderTechItem = () => {
         </select>
         <button>Submit</button>
       </form>
-    </div>
+    </StyledDiv>
   );
 };
 
